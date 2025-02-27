a22611a4b2335ec55c54dd1c6da15a7def572c54
teddy <ykcode@163.com>
2025-02-27 22:35:47 +0800

    日常更新文章

---
+++ js/baidu-tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function () {
+  var hm = document.createElement('script');
+  hm.src = 'https://hm.baidu.com/hm.js?c259ca5d6b73ce80107cf6adb1e4b3dc';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(hm, s);
+})();
